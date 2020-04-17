a1a9e26f16a6f1680a440596c42aba59732ac9ab
Seb <git@erhardt.net>
2020-04-17 16:55:47 -0700

    +/- try to fix history

---
+++ remove_andbuild.sh
@@ -0,0 +1,5 @@
+docker stop stellar
+docker rm stellar
+docker rmi stellar
+docker build -t stellar .
+docker run -t --env POSTGRES_PASSWORD="stellar" -p 8000:8000 -p 11625:11625 -p 11626:11626 -p 5432:5432 -p 1570:1570 --name stellar -d stellar --testnet
