0bb01acf0bb73b59a31c602deb3964369a72537e
Seb <git@erhardt.net>
2020-04-20 17:35:01 -0700

    +/- ports remove and rebuild

--- remove_andbuild.sh
+++ remove_andbuild.sh
@@ -2,4 +2,4 @@ docker stop stellar
 docker rm stellar
 docker rmi stellar
 docker build -t stellar .
-docker run -t --env POSTGRES_PASSWORD="stellar" -p 8000:8000 -p 11625:11625 -p 11626:11626 -p 5432:5432 -p 1570:1570 --name stellar -d stellar --testnet
+docker run -t --env POSTGRES_PASSWORD="stellar" -p 8000:8000 -p 11625:11625 -p 11626:11626 -p 5432:5432 --name stellar -d stellar --testnet
